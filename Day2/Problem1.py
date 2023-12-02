c43a167bbe93d08f46b08f4165031fa379b19c03
Nyllea <walid.outtaleb@ensta-paris.fr>
2023-12-02 10:19:27 +0100

    Day 2 solved

--- Day2/Problem1.py
+++ Day2/Problem1.py
@@ -1,7 +1,40 @@
-input_file_path = "Day2/test.txt"
+input_file_path = "Day2/input.txt"
+
+amount_of_cubes = [("red", 12), ("green", 13), ("blue", 14)]
 
 f = open(input_file_path, "r")
 lines = f.read().splitlines()
 
+valid_ids_sum = 0
 for line in lines:
-    print(line)
+    game_line = line.split(":")
+
+    game_id = int(game_line[0].split(" ")[-1])
+    
+    game_results = game_line[-1].split(";")
+    
+    game_is_valid = True
+    for result in game_results:
+        result_cubes = result.split(",")
+        for cube in result_cubes:
+            cube_values = cube.split(" ")
+            cube_amount = int(cube_values[1])
+            cube_color = cube_values[-1]
+
+            color_is_valid = False
+            for i in range(len(amount_of_cubes)):
+                if cube_color == amount_of_cubes[i][0]:
+                    color_is_valid = True
+                    if cube_amount > amount_of_cubes[i][-1]:
+                        game_is_valid = False
+                        break
+            
+            if not color_is_valid:
+                print("The color ", cube_color, " from game ", game_id, " is unknown")
+                exit()
+            
+    if game_is_valid:
+        valid_ids_sum += game_id
+        #print(game_id)
+
+print(valid_ids_sum)            
--- Day2/Problem2.py
+++ Day2/Problem2.py
@@ -1,7 +1,45 @@
-input_file_path = "Day2/test.txt"
+input_file_path = "Day2/input.txt"
+
+possible_colors = ["red", "green", "blue"]
 
 f = open(input_file_path, "r")
 lines = f.read().splitlines()
 
+sum_of_powers = 0
 for line in lines:
-    print(line)
+    game_line = line.split(":")
+
+    game_id = int(game_line[0].split(" ")[-1])
+    
+    game_results = game_line[-1].split(";")
+
+    min_cubes_needed = []
+    for color in possible_colors:
+        min_cubes_needed += [(color, 0)]
+    
+    for result in game_results:
+        result_cubes = result.split(",")
+        for cube in result_cubes:
+            cube_values = cube.split(" ")
+            cube_amount = int(cube_values[1])
+            cube_color = cube_values[-1]
+
+            color_is_valid = False
+            for i in range(len(min_cubes_needed)):
+                if cube_color == min_cubes_needed[i][0]:
+                    color_is_valid = True
+                    new_amount = max(min_cubes_needed[i][-1], cube_amount)
+                    min_cubes_needed[i] = (cube_color, new_amount)
+            
+            if not color_is_valid:
+                print("The color ", cube_color, " from game ", game_id, " is unknown")
+                exit()
+            
+    game_power = 1
+    for (min_color, min_amount) in min_cubes_needed:
+        game_power *= min_amount
+        #print(game_id, min_color, min_amount)
+    
+    sum_of_powers += game_power
+
+print(sum_of_powers)            
