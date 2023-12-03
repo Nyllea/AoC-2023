245f8a83017ee6bf6741a8f8646d3a1e2fda19ff
Nyllea <walid.outtaleb@ensta-paris.fr>
2023-12-03 19:27:33 +0100

    Use of dictionnaries for Day 2 problems

--- Day2/Problem1.py
+++ Day2/Problem1.py
@@ -1,7 +1,7 @@
 input_file_path = "Day2/input.txt"
 
 # Maximum amount of each cube for a game to be valid
-amount_of_cubes = [("red", 12), ("green", 13), ("blue", 14)]
+amount_of_cubes = {"red": 12, "green": 13, "blue": 14}
 
 f = open(input_file_path, "r")
 lines = f.read().splitlines()
@@ -17,24 +17,25 @@ for line in lines:
     
     game_is_valid = True
     for cube_set in game_results:
+        if not game_is_valid:
+            continue
+
         cubes = cube_set.split(",")
         for cube in cubes:
             cube_values = cube.split(" ")
             cube_amount = int(cube_values[1])
             cube_color = cube_values[-1]
-
-            color_is_valid = False # To make sure the cube is not an unprovided color
-            for i in range(len(amount_of_cubes)):
-                if cube_color == amount_of_cubes[i][0]:
-                    color_is_valid = True
-                    if cube_amount > amount_of_cubes[i][-1]:
-                        game_is_valid = False
-                        break
             
-            if not color_is_valid:
+            # Make sure the cube is not an unprovided color
+            if not cube_color in amount_of_cubes:
                 print("The color ", cube_color, " from game ", game_id, " is unknown")
                 exit()
             
+            # The game is not valid if it requires more cube of this color than the amount provided
+            if cube_amount > amount_of_cubes[cube_color]:
+                game_is_valid = False
+                break
+        
     if game_is_valid:
         valid_ids_sum += game_id
 
--- Day2/Problem2.py
+++ Day2/Problem2.py
@@ -1,6 +1,7 @@
 input_file_path = "Day2/input.txt"
 
-possible_colors = ["red", "green", "blue"]
+# All the possible cube colors, with their default amont to 0
+possible_colors = {"red": 0, "green": 0, "blue": 0}
 
 f = open(input_file_path, "r")
 lines = f.read().splitlines()
@@ -15,9 +16,7 @@ for line in lines:
     game_results = game_line[-1].split(";")
 
     # Minimum amount of cube of each color needed for this game to be possible
-    min_cubes_needed = []
-    for color in possible_colors:
-        min_cubes_needed += [(color, 0)]
+    min_cubes_needed = possible_colors.copy()
     
     for cube_set in game_results:
         result_cubes = cube_set.split(",")
@@ -26,22 +25,17 @@ for line in lines:
             cube_amount = int(cube_values[1])
             cube_color = cube_values[-1]
 
-            color_is_valid = False # To make sure the cube is not an unprovided color
-            for i in range(len(min_cubes_needed)):
-                if cube_color == min_cubes_needed[i][0]:
-                    color_is_valid = True
-
-                    # Set the minimum amount of cube needed for this set to also be possible
-                    new_amount = max(min_cubes_needed[i][-1], cube_amount)
-                    min_cubes_needed[i] = (cube_color, new_amount)
-            
-            if not color_is_valid:
+            # Make sure the cube is not an unprovided color
+            if not cube_color in min_cubes_needed:
                 print("The color ", cube_color, " from game ", game_id, " is unknown")
                 exit()
-            
+
+            # Actualize the minimum amount of cubes of this color needed for this game
+            min_cubes_needed[cube_color] = max(min_cubes_needed[cube_color], cube_amount)
+        
     # Calculate the power of the minimum set of cubes of the game
     game_power = 1
-    for (min_color, min_amount) in min_cubes_needed:
+    for min_amount in min_cubes_needed.values():
         game_power *= min_amount
     
     sum_of_powers += game_power
