60660a4206a5a96b797dbe7efa46c7261ff7ea76
Nyllea <walid.outtaleb@ensta-paris.fr>
2023-12-05 10:44:12 +0100

    Solutions  and comments for Day 5

--- Day5/Problem1.py
+++ Day5/Problem1.py
@@ -1,7 +1,49 @@
-input_file_path = "Day5/test.txt"
+input_file_path = "Day5/input.txt"
 
 f = open(input_file_path, "r")
 lines = f.read().splitlines()
 
+# Two lists containing the values we are looking for in each map from the input
+values_to_look_for = []
+next_values_to_look_for = []
+
 for line in lines:
-    print(line)
+    title_line = line.split(":")
+
+    # If the line is a title
+    if len(title_line) == 2:
+        seed_line = title_line[1].split(" ")
+
+        # If it's the seed line, we add the seeds to the list of values we will be looking for
+        if len(seed_line) > 1:
+            for seed_nbr in seed_line:
+                if seed_nbr.isdigit():
+                    next_values_to_look_for += [int(seed_nbr)]
+        else:
+            # We add the values that weren't mapped as they are
+            next_values_to_look_for += values_to_look_for
+
+            # We actualize the values we are looking for
+            values_to_look_for = next_values_to_look_for
+            next_values_to_look_for = []
+    else:
+        numbers = line.split(" ")
+
+        # If it's a valid line (and not an empty one)
+        if len(numbers) == 3:
+            map_destination_start = int(numbers[0])
+            map_source_start = int(numbers[1])
+            map_range_len = int(numbers[2])
+
+            temp_values_to_look_for = values_to_look_for.copy()
+            for look_for_val in temp_values_to_look_for:
+                # If the value we are looking for falls within the source range of the map,
+                # we add it's mapped value to the values we will be looking for in the next map,
+                # and remove that value from the values we are looking for
+                if look_for_val >= map_source_start and look_for_val < map_source_start + map_range_len:
+                    next_values_to_look_for += [map_destination_start + look_for_val - map_source_start]
+                    values_to_look_for.remove(look_for_val)
+
+values_to_look_for += next_values_to_look_for
+
+print(min(values_to_look_for))
--- Day5/Problem2.py
+++ Day5/Problem2.py
@@ -1,7 +1,75 @@
-input_file_path = "Day5/test.txt"
+input_file_path = "Day5/input.txt"
 
 f = open(input_file_path, "r")
 lines = f.read().splitlines()
 
+# Two lists containing the ranges of values we are looking for in each map from the input
+# in the form (range_start, range_lenght)
+ranges_to_look_for = []
+next_ranges_to_look_for = []
+
 for line in lines:
-    print(line)
+    title_line = line.split(":")
+
+    # If the line is a title
+    if len(title_line) == 2:
+        seed_line = title_line[1].split(" ")
+        seed_line.remove('')
+
+        # If it's the seed line, we look for it, we add the seeds ranges to the list of values we will be looking for
+        if len(seed_line) > 1:
+            for i in range(len(seed_line)//2):
+                seed_start = int(seed_line[2*i])
+                seed_range = int(seed_line[2*i+1])
+                    
+                next_ranges_to_look_for += [(seed_start, seed_range)]
+        else:
+            # We add the ranges that weren't mapped as they are
+            next_ranges_to_look_for += ranges_to_look_for
+
+            # We actualize the ranges we are looking for
+            ranges_to_look_for = next_ranges_to_look_for
+            next_ranges_to_look_for = []
+    else:
+        numbers = line.split(" ")
+
+        # If it's a valid line (and not an empty one)
+        if len(numbers) == 3:
+            map_destination_start = int(numbers[0])
+            map_source_start = int(numbers[1])
+            map_range_len = int(numbers[2])
+
+            temp_ranges_to_look_for = ranges_to_look_for.copy()
+            for (range_val_start, range_val_len) in temp_ranges_to_look_for:
+                # If there is an overlap of the range we are looking for and the mapped range
+                if not (range_val_start >= map_source_start + map_range_len or range_val_start + range_val_len <= map_source_start):
+                    # Calculate the part of the range we are looking for that is mapped
+                    range_start_mapped = max(range_val_start, map_source_start)
+                    range_start_destination_mapped = map_destination_start + range_start_mapped - map_source_start
+                    range_len_mapped = min(map_source_start + map_range_len, range_val_start + range_val_len) - range_start_mapped
+
+                    # Calculate the portion of the range before the mapped range
+                    range_start_unmapped_before = range_val_start
+                    range_len_unmapped_before = map_source_start - range_val_start
+
+                    # Calculate the portion of the range after the mapped range
+                    range_start_unmapped_after = map_source_start + map_range_len
+                    range_len_unmapped_after = range_val_start + range_val_len - (map_source_start + map_range_len)
+
+                    # Add the mapped range to the list of ranges we will be looking for
+                    next_ranges_to_look_for += [(range_start_destination_mapped, range_len_mapped)]
+                    ranges_to_look_for.remove((range_val_start, range_val_len))
+
+                    # Add the range before the mapped range to the list of ranges we are still looking for
+                    if range_len_unmapped_before > 0:
+                        ranges_to_look_for += [(range_start_unmapped_before, range_len_unmapped_before)]
+                    
+                    # Add the range after the mapped range to the list of ranges we are still looking for
+                    if range_len_unmapped_after > 0:
+                        ranges_to_look_for += [(range_start_unmapped_after, range_len_unmapped_after)]
+
+
+ranges_to_look_for += next_ranges_to_look_for
+
+min_value = min(ranges_to_look_for)
+print(min_value[0])
