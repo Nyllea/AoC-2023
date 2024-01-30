36730c833c2d0e5a4cd05f4927fe513562ff105f
Nyllea <walid.outtaleb@ensta-paris.fr>
2024-01-30 20:35:50 +0100

    Day 15 - Problem 1 solved

--- Day15/Problem1.py
+++ Day15/Problem1.py
@@ -1,7 +1,18 @@
-input_file_path = "Day15/test.txt"
+input_file_path = "Day15/input.txt"
 
 f = open(input_file_path, "r")
 lines = f.read().splitlines()
 
+def hash_algo(char_string):
+    current_value = 0
+    for character in char_string:
+        current_value = ((current_value + ord(character)) * 17) % 256
+    return current_value
+
+hash_sum = 0
 for line in lines:
-    print(line)
+    init_sequence = line.split(',')
+    for step in init_sequence:
+        hash_sum += hash_algo(step)
+
+print(hash_sum)
